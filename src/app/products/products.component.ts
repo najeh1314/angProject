7cd4023c42d9293d26f36ffc19946816a53af490
Najeh <mn.najeh1314@gmail.com>
2024-01-12 19:55:58 +0100

    update 9.0

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
-
+  
+  public check(obj: any) {
+    obj.checked = !obj.checked;
+  }
+  products: Array<any> =[
+    {id:1, name :"Computer",price:450.00, checked:false},
+    {id:2, name :"Printer",price:50.00, checked:false},
+    {id:3, name :"Phone",price:250.00, checked:false}
+  ];
 }
